f3071d8266cb14b4a7da542c0edbde714faa1f3b
Jordi Pun <jordipun@Jordis-MacBook-Pro.local>
2020-02-19 22:19:16 -0700

    Added changes to App.js that takes user input and submits on enter

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# lol_react_app
-A React app that uses the LoL API to display player stats
--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [SummonerName,getSummonerText] = useState('');
+
+  function handleOnChange(event) {
+    getSummonerText(event.target.value);
+  }
+  function handleOnSubmit(e){
+    alert(SummonerName);
+    e.preventDefault();
+
+  }
+
   return (
     <div className="App">
-        <p>
-          League React App
-        </p>
-      
+      <form onSubmit={handleOnSubmit}>
+        <div className="HeaderSearch"style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+              <input type="text" name="textsummonerName" onChange={handleOnChange}>
+              </input>
+        </div>
+      </form>
     </div>
   );
 }
