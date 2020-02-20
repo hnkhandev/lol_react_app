0d5b3f0767d431c7eaf0c7a6eaf8b16637a8cae8
Hussain <hnkhandev@gmail.com>
2020-02-20 02:12:34 -0700

    Moved API call to main component to be handled in onSubmit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
+import GetSummoner from './getSummoner'
 
 function App() {
-  const [SummonerName,getSummonerText] = useState('');
+  const [summonerName, setSummonerName] = useState('')
+  const [summonerData, setSummonerData] = useState({})
 
-  function handleOnChange(event) {
-    getSummonerText(event.target.value);
+  const getSummoner = async (endpoint) => {
+    const headers = {
+        'Access-Control-Allow-Origin':'*',
+        'Access-Control-Allow-Credentials':'true',
+    }
+
+    const request = {
+      method: 'GET',
+      headers: headers
+    }
+
+    const response = await fetch(endpoint, request)
+    const data = await response.json()
+    setSummonerData(data)
   }
-  function handleOnSubmit(e){
-    alert(SummonerName);
-    e.preventDefault();
 
+  const handleOnChange = (event) => {
+    setSummonerName(event.target.value)
+  }
+  
+  const handleOnSubmit = (event) => {
+    event.preventDefault()
+    async function fetchData() {
+      getSummoner(`https://cors-anywhere.herokuapp.com/https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=RGAPI-89ad4586-d8f3-4d8f-b0b0-cf8463be5e83`)
+    }
+    fetchData()
   }
 
   return (
-    <div className="App">
+    <div className='App'>
       <form onSubmit={handleOnSubmit}>
-        <div className="HeaderSearch"style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
-              <input type="text" name="textsummonerName" onChange={handleOnChange}>
-              </input>
+        <div className='summonerSearch' style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+              <input type='text' name='summonerName' onChange={handleOnChange}/>
         </div>
+        <GetSummoner data={summonerData}/>
       </form>
     </div>
   );
 }
 
-export default App;
+export default App
--- src/getSummoner.js
+++ src/getSummoner.js
@@ -1,40 +1,15 @@
 import React, {useState, useEffect} from 'react'
 
 function GetSummoner(props) {
-    const summonerName = props.name
-    const [summonerData, setSummonerData] = useState({})
-
-    const getSummoner = async (endpoint) => {
-        const headers = {
-            'Access-Control-Allow-Origin':'*',
-            'Access-Control-Allow-Credentials':'true',
-        }
+    const summonerData = props.data
     
-        const request = {
-          method: 'GET',
-          headers: headers
-        }
-    
-        const response = await fetch(endpoint, request)
-        const data = await response.json()
-        setSummonerData(data)
-    }
-
-    useEffect( () => {
-        async function fetchData() {
-            getSummoner(`https://cors-anywhere.herokuapp.com/https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=RGAPI-89ad4586-d8f3-4d8f-b0b0-cf8463be5e83`)
-        }
-        fetchData()
-    }, [])
-
     return (
-        <div>
+        <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
             Summoner name: {summonerData.name}
             <br/>
             Summoner level: {summonerData.summonerLevel}
         </div>
     )
 }
-
 
 export default GetSummoner;
