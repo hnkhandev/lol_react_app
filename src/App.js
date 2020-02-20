6b1697feb6bb4dcf3ec0a1803287fae2680b5cc6
Hussain <hnkhandev@gmail.com>
2020-02-19 19:53:57 -0700

    Set up starter React app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+        <p>
+          League React App
+        </p>
+      
+    </div>
+  );
+}
+
+export default App;
